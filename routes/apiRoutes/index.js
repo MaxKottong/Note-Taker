6541aa6c96575164b1ee93ecc74bf72eb8e3312b
Max Kottong <max.kottong@gmail.com>
2022-08-13 13:32:43 -0400

    Added router get post and delete for notes functions in the apiRoutes folder, adding delete early to see if I can make it work at once

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,31 @@
+const { getNotes, addNote, deleteNote } = require('../../lib/notes');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    let result = getNotes();
+    console.log(result);
+    res.json(result)
+});
+
+router.post('/notes', (req, res) => {
+    if (!req.body) {
+        res.sendStatus(406);
+    }
+
+    console.log(req.body);
+    let result = addNote(req.body);
+
+    res.json(result);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    if (!req.query) {
+        res.sendStatus(406);
+    }
+
+    console.log(req.params.id);
+    deleteNote(req.params.id);
+    res.sendStatus(200);
+});
+
+module.exports = router;
